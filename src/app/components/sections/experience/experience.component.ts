dece426ebdd6bff1698bf6dde5249dc1b3e4ef97
Cabrera Navarro, Kevin Oswaldo <kevin.cabrera@astrazeneca.com>
2021-12-15 21:28:24 -0600

    finish desktop view for experience step

--- src/app/components/sections/experience/experience.component.ts
+++ src/app/components/sections/experience/experience.component.ts
@@ -1,3 +1,4 @@
+import { Experience } from './../../../models/experience';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./experience.component.scss']
 })
 export class ExperienceComponent {
-  experiences = [];
+  experiences: Experience[] = [
+    {
+      title: 'Software Engineer, Backend and Frontend',
+      startDate: 'June 2020',
+      endDate: 'Present'
+    },
+    {
+      title: 'Software Engineer, Secondment',
+      startDate: 'August 2020',
+      endDate: 'May 2021'
+    },
+    {
+      title: 'Software Engineer, Backend and Frontend',
+      startDate: 'June 2019',
+      endDate: 'June 2020'
+    },
+    {
+      title: 'Software Engineer, Frontend (Halftime)',
+      startDate: 'January 2019',
+      endDate: 'June 2019'
+    },
+  ];
 }
---
+++ src/app/models/experience.ts
@@ -0,0 +1,5 @@
+export interface Experience {
+  title: string;
+  startDate: string;
+  endDate: string;
+}
