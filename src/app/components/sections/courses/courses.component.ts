d3615b3ffa5678611ce2ba6e86c1ecb8c475e388
Cabrera Navarro, Kevin Oswaldo <kevin.cabrera@astrazeneca.com>
2021-12-15 20:51:05 -0600

    Add title to all sections

--- src/app/components/sections/courses/courses.component.ts
+++ src/app/components/sections/courses/courses.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class CoursesComponent {
+  courses = [];
 }
