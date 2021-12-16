52b0e2ec6d3d7d0913943658dbce18adeffe9b26
Cabrera Navarro, Kevin Oswaldo <kevin.cabrera@astrazeneca.com>
2021-12-15 20:28:30 -0600

    Add responsive design to home

--- src/app/components/sections/skills/skills.component.ts
+++ src/app/components/sections/skills/skills.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.scss']
 })
-export class SkillsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class SkillsComponent {
+  skills = [
+    'Typescript',
+    'Javascript',
+    'Java',
+    'NodeJS',
+    'Angular',
+    'Springboot',
+    'HTML',
+    'SCSS',
+    'PostgreSQL',
+    'JUnit5',
+    'Jasmine',
+    'Python'
+  ];
+  otherSkills = [
+    'GIT',
+    'Mocha',
+    'Jira',
+    'Confluence',
+    'Myro',
+    'Cypress',
+    'Docker',
+    'Postman',
+    'Agile methodologies',
+    'AdobeXD',
+    'Zapworks Studio',
+    'Power Apps',
+    'Microsoft suite'
+  ];
 }
