c0da895684f4f2fb3aacf43d9d8520f74240a043
Cabrera Navarro, Kevin Oswaldo <kevin.cabrera@astrazeneca.com>
2021-12-15 19:36:36 -0600

    Add footer component to all sections

--- src/app/components/reusable/buttons-bottom-bar/buttons-bottom-bar.component.ts
+++ src/app/components/reusable/buttons-bottom-bar/buttons-bottom-bar.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-buttons-bottom-bar',
   templateUrl: './buttons-bottom-bar.component.html',
   styleUrls: ['./buttons-bottom-bar.component.scss']
 })
-export class ButtonsBottomBarComponent implements OnInit {
+export class ButtonsBottomBarComponent {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  goBack(): void {}
+
+  goNext(): void {}
 
 }
