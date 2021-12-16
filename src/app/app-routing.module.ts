0c8a2e5161c59ad9f4f3a5b7c13bb4135086fb9b
Cabrera Navarro, Kevin Oswaldo <kevin.cabrera@astrazeneca.com>
2021-12-15 19:11:27 -0600

    Add shadow to toolbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { HomeComponent } from './components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { CoursesComponent } from './components/courses/courses.component';
-import { LanguagesComponent } from './components/languages/languages.component';
-import { EducationComponent } from './components/education/education.component';
-import { ExperienceComponent } from './components/experience/experience.component';
-import { SkillsComponent } from './components/skills/skills.component';
+import { ContactComponent } from './components/sections/contact/contact.component';
+import { CoursesComponent } from './components/sections/courses/courses.component';
+import { LanguagesComponent } from './components/sections/languages/languages.component';
+import { EducationComponent } from './components/sections/education/education.component';
+import { ExperienceComponent } from './components/sections/experience/experience.component';
+import { SkillsComponent } from './components/sections/skills/skills.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
