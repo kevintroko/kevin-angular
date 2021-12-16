99f135b07cb3243a5d0d372fb10893b70750eb1e
Cabrera Navarro, Kevin Oswaldo <kevin.cabrera@astrazeneca.com>
2021-12-15 19:23:57 -0600

    listens to active route

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,24 +1,35 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { TOOLBAR_TABS } from './../../constants/toolbar-tabs';
+import { Component, OnDestroy } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent {
-  tabs = [
-    { name: 'skills' },
-    { name: 'experience' },
-    { name: 'education' },
-    { name: 'languages' },
-    { name: 'courses' },
-    { name: 'contact' }
-  ];
+export class ToolbarComponent implements OnDestroy {
+  tabs = TOOLBAR_TABS;
+  tabSubscriber: Subscription | undefined;
+  activeTab: string = '';
 
-  constructor(private _router: Router) {}
+  constructor(private _router: Router) {
+    this._router.events.subscribe((event) => {
+      // Listens to active route to set the active status at toolbar
+      if (event instanceof NavigationStart) {
+        this.activeTab = event.url.split('/')[1];
+      }
+    });
+  }
 
   navigate(url: string): void {
     this._router.navigate([`/${url}`]);
+  }
+
+  ngOnDestroy(): void {
+    // Avoid subscription to the same thing multiple times
+    if (this.tabSubscriber) {
+      this.tabSubscriber.unsubscribe();
+    }
   }
 }
