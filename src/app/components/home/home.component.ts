0029a43580852b23274ff01824eeeeeef640f83c
Cabrera Navarro, Kevin Oswaldo <kevin.cabrera@astrazeneca.com>
2021-12-15 18:54:06 -0600

    Navigate to skills on click next in home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  constructor(private _route: Router) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  navigate(): void {
+    this._route.navigate(['/skills']);
   }
-
 }
