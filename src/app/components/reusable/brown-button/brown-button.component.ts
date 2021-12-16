68d35dbfcf0cd34c0ff5a11b4851ad5c8fce3bc3
Cabrera Navarro, Kevin Oswaldo <kevin.cabrera@astrazeneca.com>
2021-12-15 18:00:17 -0600

    Center image in home

--- src/app/components/reusable/brown-button/brown-button.component.ts
+++ src/app/components/reusable/brown-button/brown-button.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-brown-button',
   templateUrl: './brown-button.component.html',
   styleUrls: ['./brown-button.component.scss']
 })
-export class BrownButtonComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class BrownButtonComponent {
+  @Input() text: string = 'button';
 }
