6dbc98321f2487646decae3df59274b4968382ec
Cabrera Navarro, Kevin Oswaldo <kevin.cabrera@astrazeneca.com>
2021-12-15 22:05:47 -0600

    add menu for small screens

--- src/module/custom-material.module.ts
+++ src/module/custom-material.module.ts
@@ -3,19 +3,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   imports: [
     CommonModule,
     MatToolbarModule,
     MatRippleModule,
-    MatIconModule
+    MatIconModule,
+    MatMenuModule
   ],
   exports: [
     CommonModule,
     MatToolbarModule,
     MatRippleModule,
-    MatIconModule
+    MatIconModule,
+    MatMenuModule
   ],
   declarations: [],
   bootstrap: []
