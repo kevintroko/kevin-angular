25b611ef89a9cd72e6dec83c9cb99e07e00a224b
Cabrera Navarro, Kevin Oswaldo <kevin.cabrera@astrazeneca.com>
2021-12-15 21:43:50 -0600

    hide menu in small menus

--- src/module/custom-material.module.ts
+++ src/module/custom-material.module.ts
@@ -2,17 +2,20 @@ import { MatRippleModule } from '@angular/material/core';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   imports: [
     CommonModule,
     MatToolbarModule,
-    MatRippleModule
+    MatRippleModule,
+    MatIconModule
   ],
   exports: [
     CommonModule,
     MatToolbarModule,
-    MatRippleModule
+    MatRippleModule,
+    MatIconModule
   ],
   declarations: [],
   bootstrap: []
