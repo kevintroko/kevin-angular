0df556375465299f4dade408b4c6f6e2ff459f12
Cabrera Navarro, Kevin Oswaldo <kevin.cabrera@astrazeneca.com>
2021-12-15 22:37:48 -0600

    add image of university to education tab add univesity interface

--- src/app/components/sections/experience/experience.component.ts
+++ src/app/components/sections/experience/experience.component.ts
@@ -19,12 +19,12 @@ export class ExperienceComponent {
       endDate: 'May 2021'
     },
     {
-      title: 'Software Engineer, Backend and Frontend',
+      title: 'Trainee Software Engineer, Backend and Frontend',
       startDate: 'June 2019',
       endDate: 'June 2020'
     },
     {
-      title: 'Software Engineer, Frontend (Halftime)',
+      title: 'Intern Software Engineer, Frontend (Halftime)',
       startDate: 'January 2019',
       endDate: 'June 2019'
     },
